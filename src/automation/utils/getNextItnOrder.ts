6b7b598162525d56f730ac7396cde22bfe0fe192
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-09-18 16:00:07 -0300

    Revert "[refactor] ♻️: refactor function for get next order"
    This reverts commit d9728145576f6382b0a8c4989375cf871a52fcaa.

--- src/automation/utils/getNextItnOrder.ts
+++ src/automation/utils/getNextItnOrder.ts
@@ -1,9 +1,11 @@
+import { SaramsNotFound } from "../../core/errors/repositoryErrors/SaramsNotFound";
+
 export function getNextItnOrder(sarams: string[]) {
   let _arrOrders = [...sarams];
-  return function () {
+  return function (): string {
     if (!_arrOrders.length) _arrOrders = [...sarams];
     const nextOrder = _arrOrders.shift();
-    if (typeof nextOrder === "string") return nextOrder;
-    return;
+    if (!nextOrder) throw new SaramsNotFound();
+    return nextOrder;
   };
 }
