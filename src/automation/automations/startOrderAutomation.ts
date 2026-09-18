f8f4612827bf87dcb5e76f27b6144fbd9d94269c
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-09-18 16:59:32 -0300

    [refactor] ♻️: refactor type error and handling error for timeout errors

--- src/automation/automations/startOrderAutomation.ts
+++ src/automation/automations/startOrderAutomation.ts
@@ -1,9 +1,14 @@
 import type { Page } from "playwright";
+import { AutomationTimeoutError } from "../../core/errors/automationErrors/AutomationTimeoutError";
 
 export async function startOrderAutomation(page: Page) {
-  await page.locator("#sistema_titulo").click();
-  await page.locator("#j_idt20").click();
-  await page.locator(".fa.fa-truck").click();
-  await page.locator(".fa.fa-calendar-check-o").click();
-  await page.locator(".fa.fa-car").click();
+  try {
+    await page.locator("#sistema_titulo").click();
+    await page.locator("#j_idt20").click();
+    await page.locator(".fa.fa-truck").click();
+    await page.locator(".fa.fa-calendar-check-o").click();
+    await page.locator(".fa.fa-car").click();
+  } catch (err) {
+    throw new AutomationTimeoutError();
+  }
 }
